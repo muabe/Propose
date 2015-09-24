0945f86f4e90575ae57d42227a6a9a719f78ed06
오재웅 <markjmind@gmail.com>
2015-09-24 17:56:26 +0900

    소스 위치 변경

--- sample/paper/src/com/markjmind/propose/sample/paper/MainActivity.java
+++ sample/paper/src/com/markjmind/propose/sample/paper/MainActivity.java
@@ -18,14 +18,14 @@ public class MainActivity extends Activity{
 		/** Layout init **/
 		ViewGroup move_lyt = (ViewGroup)findViewById(R.id.move_lyt);
 		ViewGroup book_lyt = (ViewGroup)findViewById(R.id.book_lyt);
-		
+		move_lyt.setCameraDistance(Propose.getCameraDistanceY(this)*5);
+		book_lyt.setCameraDistance(Propose.getCameraDistanceY(this)*5);
+				
 		/** create animator **/
 		ObjectAnimator paperAnim = ObjectAnimator.ofFloat(move_lyt, View.ROTATION_Y, 0,180);
 		ObjectAnimator paperUpDown = ObjectAnimator.ofFloat(move_lyt, View.ROTATION_X, -50,50);
 		ObjectAnimator bookUpDown = ObjectAnimator.ofFloat(book_lyt, View.ROTATION_X, -50,50);
-		move_lyt.setCameraDistance(Propose.getCameraDistanceY(this)*5);
-		book_lyt.setCameraDistance(Propose.getCameraDistanceY(this)*5);
-		
+				
 		/** create propose **/
 		Propose propose = new Propose(this);
 		propose.motionRight.play(paperAnim); 					 /** set right move animator **/ 
