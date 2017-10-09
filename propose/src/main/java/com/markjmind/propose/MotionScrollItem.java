3ed95413845a5e8ef17af80f9dca42c4aeba965c
Muabe <markjmind@gmail.com>
2017-10-09 22:26:40 +0900

    MotionScrollItem 코드 정리

--- propose/src/main/java/com/markjmind/propose/MotionScrollItem.java
+++ propose/src/main/java/com/markjmind/propose/MotionScrollItem.java
@@ -9,7 +9,11 @@ import com.markjmind.propose.listener.AnimatorAdapter;
 /**
  * <br>捲土重來<br>
  *
- *
+ * 스크롤(드래그)에 따른 애니메이션의 진행상태 감지하고 이벤트를 발생히주는 구현체 클래스이다.<br>
+ * 애니메이션 진행상태는 애니메이션의 준비상태와 시작상태이며<br>
+ * 준비상태는 아직 애니메이션이 play되지 않은 상태를 말한다.<br>
+ * 애니메이션의 상태를 구분함과 동시에 애니메이션의 Duration을 싱크해준다.<br>
+ * 애니메이션의 상태는 스테이트 패턴으로 되어있어 ScrollState로 상태를 추가 확장할수 있다.
  *
  * @author 오재웅(JaeWoong-Oh)
  * @email markjmind@gmail.com
@@ -17,20 +21,62 @@ import com.markjmind.propose.listener.AnimatorAdapter;
  */
 @SuppressLint("NewApi") 
 public class MotionScrollItem {
+	/** 추가되는 Duration*/
 	protected long joinDuration = 0;
+	/** 애니메이션 시작전 delay Duration*/
 	protected long delayDuration = 0;
+	/** 실제 애니메이션이 play시작되는 시점*/
 	protected long startDuration = 0;
+	/** 총 애니메이션 Duration(애니메이션의 끝과 같다) */
 	protected long endDuration = 0;
+	/** delay 등을 제외한 순수 애니메이션 Duration*/
 	protected long playDuration = 0;
 
+	/** 애니메이션 */
 	private ValueAnimator anim;
 	
-	
+	/** 현재 Duration 수치*/
 	private long currDuration = 0;
 
+	/** 현재 진행상태 */
 	private ScrollState currScrollState;
+	/** 진행상태 */
 	private ScrollState forwardReady, forwardStart, reverseReady, reverseStart;
 
+
+	/**
+	 * 애니메이션 진행상태 인터페이스<br>
+	 * scroll : 진행중때 언제나 발생<br>
+	 * between : 드래그가 현재 아이템의 duration 범위 안에서 진행중 일때 처리<br>
+	 * over : 드래그가 현재 아이템의 duration 범위를 벗어 났을때 처리<br>
+	 * lesser :드래그가 현재 아이템의 duration 범위에 못들어 올때 처리 <br>
+	 */
+	abstract class ScrollState{
+		public boolean isReverse;
+		public ScrollState(boolean isReverse){
+			this.isReverse = isReverse;
+		}
+		public boolean scroll(int index){
+			boolean result = false;
+			if(0<currDuration && currDuration<playDuration){
+				result =  between(index);
+			}else if(currDuration>=playDuration){
+				result =  over(index);
+			}else{
+				result =  lesser(index);
+			}
+			return result;
+		}
+		public abstract boolean between(int index);
+		public abstract boolean over(int index);
+		public abstract boolean lesser(int index);
+	}
+
+	/**
+	 * 기본 생성자
+	 * @param animator 애니메이션
+	 * @param joinDuration 애니메이션의 Duration
+	 */
 	public MotionScrollItem(ValueAnimator animator, long joinDuration){
 		this.anim = animator;
 
@@ -40,6 +86,7 @@ public class MotionScrollItem {
 		this.startDuration = joinDuration+delayDuration;
 		this.endDuration = startDuration+playDuration;
 
+		/** 애니메이션의 준비상태 정의 */
 		forwardReady = new ScrollState(false) {
 			@Override
 			public boolean between(int index) {
@@ -68,6 +115,7 @@ public class MotionScrollItem {
 			}
 		};
 
+		/** 애니메이션의 시작상태 정의*/
 		forwardStart = new ScrollState(false) {
 			@Override
 			public boolean between(int index) {
@@ -96,6 +144,8 @@ public class MotionScrollItem {
 				return true;
 			}
 		};
+
+		/** 반대방향 애니메이션의 준비상태 정의*/
 		reverseStart = new ScrollState(true) {
 			@Override
 			public boolean between(int index) {
@@ -124,6 +174,8 @@ public class MotionScrollItem {
 				return true;
 			}
 		};
+
+		/** 반대방향 애니메이션의 시작상태 정의*/
 		reverseReady = new ScrollState(true) {
 			@Override
 			public boolean between(int index) {
@@ -153,13 +205,24 @@ public class MotionScrollItem {
 		};
 		this.currScrollState = forwardReady;
 	}
-	
+
+
+	/**
+	 * 진행 상태와 애니메이션의 duration을 초기화한다.
+	 */
 	protected void reset(){
 		currDuration = 0;
 		this.currScrollState = forwardReady;
 		anim.setCurrentPlayTime(currDuration);
 	}
-	
+
+
+	/**
+	 * 외부에서 드래그시 진행상태를 변경할때 사용한다.
+	 * @param index 모션의 인덱스
+	 * @param duration 진행할 duration
+	 * @return 이벤트 발생 여부
+	 */
 	public boolean scroll(int index, long duration){
 		currDuration = duration-startDuration;
 
@@ -171,6 +234,13 @@ public class MotionScrollItem {
 
 	}
 
+
+	/**
+	 * 애니메이션의 시작과 끝의 이벤트를 발생시켜주는 구현체
+	 * @param index 모션의 인덱스
+	 * @param start 애니메이션의 시작여부(false일 경우 애니메이션 종료)
+	 * @param isForward 애니메이션의 방향(false일 경우 반대방향)
+	 */
 	private void onEvent(int index, boolean start, boolean isForward){
 		if(anim.getListeners()!=null){
 			for(int i=0;i<anim.getListeners().size();i++){
@@ -188,13 +258,31 @@ public class MotionScrollItem {
 		}
 	}
 
+
+	/**
+	 * 외부에서 애니메이션의 시작 이벤트를 받기 위한 인터페이스
+	 * @param index 모션의 인덱스
+	 * @param isForward 애니메이션의 방향(false일 경우 반대방향)
+	 */
 	private void startEvent(int index, boolean isForward){
 		this.onEvent(index, true, isForward);
 	}
+
+	/**
+	 * 외부에서 애니메이션의 종료 이벤트를 받기 위한 인터페이스
+	 * @param index
+	 * @param isForward
+	 */
 	private void endEvent(int index, boolean isForward){
 		this.onEvent(index, false, isForward);
 	}
 
+
+	/**
+	 *
+	 * 드래그에 따라 이벤트를 발생시켜주는 구현체<br>
+	 * anim 객체에 onScroll 리스너를 등록했을 경우 이벤트를 발생시켜 준다.
+	 */
 	private void scrollEvent() {
 		if(anim.getListeners()!=null) {
 			for (int i = 0; i < anim.getListeners().size(); i++) {
@@ -206,33 +294,21 @@ public class MotionScrollItem {
 			}
 		}
 	}
-	
+
+	/**
+	 * 현지 진행되고 있는 duration을 가져온다.
+	 * @return 현재 duration 값
+	 */
 	protected long getCurrDuration(){
 		return this.currDuration;
 	}
-	
+
+	/**
+	 * 애니메이션이 끝나는 시점의 duration을 가져온다.
+	 * @return 종료 duration 값
+	 */
 	protected long getEndDuration(){
 		return this.endDuration;
 	}
 
-	abstract class ScrollState{
-		public boolean isReverse;
-		public ScrollState(boolean isReverse){
-			this.isReverse = isReverse;
-		}
-		public boolean scroll(int index){
-			boolean result = false;
-			if(0<currDuration && currDuration<playDuration){
-				result =  between(index);
-			}else if(currDuration>=playDuration){
-				result =  over(index);
-			}else{
-				result =  lesser(index);
-			}
-			return result;
-		}
-		public abstract boolean between(int index);
-		public abstract boolean over(int index);
-		public abstract boolean lesser(int index);
-	}
 }
