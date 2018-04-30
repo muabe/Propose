4c519d98076635c92d69f52a2801058ced496619
Muabe <markjmind@gmail.com>
2018-04-30 17:42:11 +0900

    Sample 모듈 추가

---
+++ sample/src/main/java/com/muabe/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.muabe.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
