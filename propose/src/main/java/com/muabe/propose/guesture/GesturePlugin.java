fc50bbe8e11cf36e3302917a49a330b69bf96788
Muabe <markjmind@gmail.com>
2018-10-18 07:50:42 +0900

    Action 부분 Frame 완료

--- propose/src/main/java/com/muabe/propose/guesture/GesturePlugin.java
+++ propose/src/main/java/com/muabe/propose/guesture/GesturePlugin.java
@@ -1,6 +1,21 @@
 package com.muabe.propose.guesture;
 
 
-public abstract class GesturePlugin{
+import com.muabe.propose.action.ModuleName;
 
+import java.lang.reflect.ParameterizedType;
+
+public abstract class GesturePlugin<EventType> implements ModuleName {
+    private String name;
+
+    public GesturePlugin(){
+        name = ((Class)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0]).getName();
+    }
+
+    @Override
+    public String getTypeName(){
+        return name;
+    }
+
+    public abstract void get(EventType eventType);
 }
