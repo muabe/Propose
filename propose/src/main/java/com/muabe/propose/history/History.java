c55752875bbc6b5ecb140136b01612413f06a3c5
muabe <markjmind@gmail.com>
2019-11-11 18:28:17 +0900

    히스토리 개발중

---
+++ propose/src/main/java/com/muabe/propose/history/History.java
@@ -0,0 +1,28 @@
+package com.muabe.propose.history;
+
+import com.muabe.propose.Motion;
+import com.muabe.propose.player.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class History {
+    HashMap<Motion, ArrayList<Player>> history = new HashMap<>();
+
+    public void addMotion(Motion motion, float ratio){
+        if(history.containsKey(motion)){
+
+        }else{
+            history.put(motion, null);
+            newMotion(motion, ratio);
+
+        }
+    }
+
+
+
+
+    private void newMotion(Motion motion, float ratio){
+
+    }
+}
---
+++ propose/src/main/java/com/muabe/propose/history/HistoryInfo.java
@@ -0,0 +1,11 @@
+package com.muabe.propose.history;
+
+import com.muabe.propose.Motion;
+import com.muabe.propose.player.Player;
+
+import java.util.ArrayList;
+
+public class HistoryInfo{
+    Motion motion;
+    ArrayList<Player> list = new ArrayList<>();
+}
