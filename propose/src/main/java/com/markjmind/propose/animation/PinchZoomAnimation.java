2b87856580eb609953441631b3c163d8c7096f04
muabe <markjmind@gmail.com>
2020-11-19 14:31:56 +0900

    pinch 플러그인 완료

--- propose/src/main/java/com/markjmind/propose/animation/PinchZoomAnimation.java
+++ propose/src/main/java/com/markjmind/propose/animation/PinchZoomAnimation.java
@@ -7,15 +7,22 @@ import android.view.View;
 
 public class PinchZoomAnimation extends ValueAnimator implements ValueAnimator.AnimatorUpdateListener {
     private View touchView;
-    private float currentScale = 1f;
-    private ScaleGestureDetector detector;
+        private ScaleGestureDetector detector;
     private long duration = 1000;
-
+    private float density;
+    private float maxPinch;
 
     public PinchZoomAnimation(View touchView){
-        detector = new ScaleGestureDetector(touchView.getContext(), new PinchDetector());
+        init();
+        density = touchView.getContext().getResources().getDisplayMetrics().density;
+        maxPinch = 100*density;
+    }
+
+    public PinchZoomAnimation init(){
+        detector = new ScaleGestureDetector(touchView.getContext(), new PinchDetector(this));
         setPinchView(touchView);
         setFloatValues(0,1f);
+        return this;
     }
 
     public PinchZoomAnimation setDuration(long duration){
@@ -36,6 +43,20 @@ public class PinchZoomAnimation extends ValueAnimator implements ValueAnimator.A
     @Override
     public void onAnimationUpdate(ValueAnimator animation) {
 
+
+    }
+
+    public float getDensity(){
+        return this.density;
+    }
+
+    public float getMaxPinch(){
+        return maxPinch;
+    }
+
+    public PinchZoomAnimation setMaxPinch(float dp){
+        maxPinch = dp*density;
+        return this;
     }
 
     public void setPinchView(View view){
@@ -49,7 +70,30 @@ public class PinchZoomAnimation extends ValueAnimator implements ValueAnimator.A
     }
 
     class PinchDetector extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        PinchZoomAnimation animation;
+        float total = 0f;
+        float totalX = 0f;
+        float totalY = 0f;
+        float preX = 0f;
+        float preY = 0f;
+
+        public PinchDetector(PinchZoomAnimation animation){
+            this.animation = animation;
+        }
         public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            float x = scaleGestureDetector.getCurrentSpanX() - preX;
+            float y = scaleGestureDetector.getPreviousSpanY() - preY;
+            totalX += x;
+            totalY += y;
+            total = totalX + totalY;
+            if(total < 0f){
+                total = 0f;
+            }else if(total > maxPinch){
+                total = maxPinch;
+            }
+            animation.setCurrentPlayTime((long)(animation.getDuration()*(total/maxPinch)));
+            preX = scaleGestureDetector.getCurrentSpanX();
+            preY = scaleGestureDetector.getPreviousSpanY();
             return true;
         }
     }
